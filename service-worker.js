fc83d3f03b067c9763ac7754f3dc3b7a31ee930c
hemanthkrishna03 <mhemanthkanna03@gmail.com>
2026-01-02 18:06:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,6 +8,20 @@ const FILES_TO_CACHE = [
   "./songs/Amma_song.mp3",
   "./songs/Aigiri_song.mp3",
   "./songs/Prema_Deshapu_yuvarani.mp3"
+  "./songs/Amma_song.mp3",
+  "./songs/Aigiri_song.mp3",
+  "./songs/Prema_Deshapu_yuvarani.mp3",
+  "./songs/laksya.mp3",
+  "./songs/npsnii.mp3",
+  "./songs/sneekevaru.mp3",
+  "./songs/raazi.mp3",
+  "./songs/vajpyee.mp3",
+  "./songs/shershaah.mp3",
+  "./songs/jawan_dharam.mp3",
+  "./songs/fighter.mp3",
+  "./songs/fighter.mp3",
+  "./songs/beast.mp3"
 
 ];
+
 
